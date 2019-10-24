95a2254c6ef1a076378039957a2767b06fb69257
asching7108 <asching7108@gmail.com>
2019-10-23 22:12:13 -0400

    completed quiz app

---
+++ index.js
@@ -0,0 +1,145 @@
+'use strict'
+
+const TOTAL_QUESTION = 10;
+const TOTAL_SCORE = 10;
+
+let progress;
+let score;
+
+/* Initially display the start view. Start the quiz when the user 
+ * clicks the start button. */
+function startQuiz() {
+  $('.js-progressAndScore').hide();
+  $('.js-questionView').hide();
+  $('.js-scoreView').hide();
+  $('.js-startButton').click(function(event) {
+    // hide the start view
+    $('.js-startView').hide();
+    // unhide the progress and score bar
+    $('.js-progressAndScore').show();
+    initializeQuiz();
+  });
+}
+
+/* Initialize the progress and score of the quiz, and display the 
+ * question view. */
+function initializeQuiz() {
+  progress = 1;
+  score = 0;
+  $('.progress').html(`Questions: ${progress}/${TOTAL_QUESTION}`);
+  $('.score').html(`Score: ${score}/${TOTAL_SCORE}`);
+  // unhide the question view
+  $('.js-nextButton').hide();
+  $('.js-evaluateButton').hide();
+  $('.js-questionView').show();
+  $('.js-answer').html('');
+  addQuestionContent();
+}
+
+/* Add the question and options for display based on current 
+ * progress. */
+function addQuestionContent() {
+  const currentQuiz = quiz[progress - 1];
+  $('.js-question').html(currentQuiz.question);
+  $('.js-questionView').find('.js-options').html('');
+  for (let i = 0; i < 4; i++) {
+    $('.js-questionView').find('.js-options').append(`<li><input type="radio" id="option${i + 1} " name="option"value="${currentQuiz.options[i]}" required>${currentQuiz.options[i]}</input></li>`);
+  }
+}
+
+/* Check the user's choice and display the answer. */
+function handleSubmit() {
+  $('.js-quiz').submit(function(event) {
+    event.preventDefault();
+    let answer = quiz[progress - 1].answer;
+    let selected = $('input[name="option"]:checked').val().replace(">", "&gt").replace("<", "&lt");
+    // display answer; update score if the user choice is correct
+    if (answer == selected) {
+      $('.js-answer').removeClass('wrong');
+      $('.js-answer').html('Well done! You got it right.');
+      updateScore();
+    }
+    else {
+      $('.js-answer').addClass('wrong');
+      $('.js-answer').html(`Too bad. The answer is ${answer}.`);
+    }
+    $('.js-submitButton').hide();
+    // if this is the last question, display evalute button,
+    // otherwise display next button
+    if (progress < TOTAL_QUESTION) {
+      $('.js-nextButton').show();
+    }
+    else {
+      $('.js-evaluateButton').show();
+    }
+  });
+}
+
+/* Hide the answer, update the progress, and display the next 
+ * question. */
+function handleNext() {
+  $('.js-nextButton').click(function(event) {
+    $('.js-submitButton').show();
+    $('.js-nextButton').hide();
+    $('.js-answer').html('');
+    updateProgress();
+    addQuestionContent();
+  });
+}
+
+/* Display the final score view. */
+function handleEvaluate() {
+  $('.js-evaluateButton').click(function(event) {
+    $('.js-submitButton').show();
+    $('.js-evaluateButton').hide();
+    // hide the question view
+    $('.js-questionView').hide();
+    // unhide the score view
+    $('.js-scoreView').show();
+    evaluateScore();
+  });
+}
+
+/* Restart the quiz. */
+function handleRestart() {
+  $('.js-restartButton').click(function(event) {
+    // hide the score view
+    $('.js-scoreView').hide();
+    initializeQuiz();
+  });
+}
+
+/* Update the progress by adding 1. */
+function updateProgress() {
+  progress++;
+  $('.progress').html(`Questions: ${progress}/${TOTAL_QUESTION}`);
+}
+
+/* Update the score by adding 1. */
+function updateScore() {
+  score++;
+  $('.score').html(`Score: ${score}/${TOTAL_SCORE}`);
+}
+
+/* Add a text of evaluation based on the score. */
+function evaluateScore() {
+  if (score >= 9) {
+    $('.js-evaluation').html("Great job! Keep up the good work!");
+  } 
+  else if (score >= 6) {
+    $('.js-evaluation').html("Not bad! Just a little more effort!");
+  }
+  else {
+    $('.js-evaluation').html("You need more practice!");
+  }
+}
+
+function handleQuizApp() {
+  startQuiz();
+  handleSubmit();
+  handleNext();
+  handleEvaluate();
+  handleRestart();
+}
+
+$(handleQuizApp());
---
+++ quiz.js
@@ -0,0 +1,101 @@
+const quiz = [
+  //1
+  {
+    question: "In HTML, colors can be specified using HSL value, which means",
+    options: [
+      "High, Static, Low",
+      "High, Stable, Low",
+      "HTML, Semantics, Language",
+      "Hue, Saturation, Light"],
+    answer: "Hue, Saturation, Light"
+  }, 
+  //2
+  {
+    question: "A tag used as an alternative to make text bold is",
+    options: [
+      "&ltbold&gt",
+      "&ltdouble&gt",
+      "&ltstrong&gt",
+      "&ltdark&gt"],
+    answer: "&ltstrong&gt"
+  },
+  //3
+  {
+    question: "Attributes provided by &ltimg&gt tag of HTML are source file, width/height and",
+    options: [
+      "&ltalt&gt",
+      "&ltlink&gt",
+      "&ltdesign&gt",
+      "&lthref&gt"],
+    answer: "&ltalt&gt"
+  },
+  //4
+  {
+    question: "Elements &ltdiv&gt and &ltspan&gt are known to be",
+    options: [
+      "HTML formatting tags",
+      "HTML grouping tags",
+      "HTML container tags",
+      "HTML listing tags"],
+    answer: "HTML container tags"
+  },
+  //5
+  {
+    question: "In HTML, a hyperlink can be added as",
+    options: [
+      "&lta href='http://www.example.com'&gtExample&lt/a&gt",
+      "&lta src='http://www.example.com'&gtexample&lt/a&gt",
+      "&lta url='http://www.example.com'&gtexample&lt/a&gt",
+      "&lta link='http://www.example.com'&gtexample&lt/a&gt"],
+    answer: "&lta href='http://www.example.com'&gtExample&lt/a&gt"
+  },
+  //6
+  {
+    question: "For highlight text, HTML uses element said to be",
+    options: [
+      "&ltem&gt",
+      "&ltimp&gt",
+      "&ltmark&gt",
+      "&lthighlight&gt"],
+    answer: "&ltem&gt"
+  },
+  //7
+  {
+    question: "CSS stands for",
+    options: [
+      "Case Sensitive Sheets",
+      "Cascading Style Sheets",
+      "Cascading Sensitive Sheets",
+      "Case Style Sheets"],
+    answer: "Cascading Style Sheets"
+  },
+  //8
+  {
+    question: "A caption for &ltfieldset&gt element can be defined through element",
+    options: [
+      "&ltcap&gt",
+      "&lttitle&gt",
+      "&ltattfield&gt",
+      "&ltlegend&gt"],
+    answer: "&ltlegend&gt"
+  },
+  //9
+  {
+    question: "Method used by JavaScript to select a HTML element is",
+    options: [
+      "document.setElementById(class)",
+      "document.setElementById(id)",
+      "document.getElementById(id)",
+      "document.getElementByClass(id)"],
+    answer: "document.getElementById(id)"
+  },
+  //10
+  {
+    question: "In HTML, language of document is declared in tag",
+    options: [
+      "&lt!DOCTYPE html&gt",
+      "&lthtml&gt",
+      "&ltbody&gt",
+      "&ltref&gt"],
+    answer: "&lthtml&gt"
+  }];
